dc58da54b86e8fb729a370fec05dc1cea99c50e2
Polina Drokina <sukhova2016@inbox.ru>
2022-11-02 11:43:35 +0400

    Добавлено решение первой задачи

---
+++ task1/Program.cs
@@ -0,0 +1,5 @@
+﻿int Num = 12821;
+
+if ((Num / 10000 == Num % 10) && ((Num / 1000) % 10 == (Num % 100) / 10)) Console.WriteLine("Число является палиндромом");
+
+else Console.WriteLine("Число не является палиндромом");
